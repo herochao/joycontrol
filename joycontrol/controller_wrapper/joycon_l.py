933e491b410b665e5e1a60b3bd23b8db793aa172
Nicolas Huaman <nicolas@herochao.de>
2020-03-10 21:28:01 +0100

    updated wrappers naming

--- joycontrol/controller_wrapper/joycon_l.py
+++ joycontrol/controller_wrapper/joycon_l.py
@@ -1,6 +1,6 @@
 from joycontrol.controller_wrapper.generic_controller import GenericController
 from joycontrol.protocol import Controller
 
-class JoyConR(GenericController):
+class JoyConL(GenericController):
     def __init__(self, capture_file=None, spi_flash=None):
-        super().__init__(Controller.JOYCON_R, capture_file, spi_flash)
+        super().__init__(Controller.JOYCON_L, capture_file, spi_flash)
